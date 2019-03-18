ce7e07cfbab859b97020381933037f42c0b6f8ea
Anthony Le Cigne <dev@lecigne.net>
2019-03-18 16:05:21 +0100

    Continue chapter 2

--- src/main/java/net/lecigne/javaprojects/stringsort/App.java
+++ src/main/java/net/lecigne/javaprojects/stringsort/App.java
@@ -5,7 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		var names = new String[] { "Johnson", "Wilson", "Wilkinson", "Abraham", "Dagobert" };
 		Sort.sort(names);
-		for (final String name : names) {
+		for (final var name : names) {
 			System.out.println(name);
 		}
 	}
